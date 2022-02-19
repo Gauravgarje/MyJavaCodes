28a7f6fd4c7ab64b29194fb3fcfb74ccb5f68149
gaurav garje <gauravgarje2000@gmail.com>
2022-02-19 18:19:10 +0530

    Print Data Types in Java

---
+++ Datatypes.java
@@ -0,0 +1,67 @@
+public class Datatypes {
+    static byte myByte;
+    static short myShort;
+    static int myInt;
+    static long myLong;
+
+    static float myFloat;
+    static double myDouble;
+    
+    static char myChar;
+    static String myString;
+    public static void main(String[] args) {
+        
+        System.out.println("DEFAULT VALUES");
+        System.out.println("Byte = " + myByte);
+        System.out.println("Short = " + myShort);
+        System.out.println("Int = " + myInt);
+        System.out.println("Long = " + myLong);
+        System.out.println("Float = " + myFloat);
+        System.out.println("Double = " + myDouble);
+        System.out.println("Char = " + myChar);
+        System.out.println("String = " + myString);
+        
+        myByte = 3;
+        myShort = 35;
+        myInt = 35;
+        myLong = 25;
+        
+        myFloat = 25f;
+        myDouble = 32.0;
+        
+        myChar = 'c';
+        myString = "this is a string";
+        
+        
+        System.out.println("MODIFIED VALUES");
+        System.out.println("Byte = " + myByte);
+        System.out.println("Short = " + myShort);
+        System.out.println("Int = " + myInt);
+        System.out.println("Long = " + myLong);
+        System.out.println("Float = " + myFloat);
+        System.out.println("Double = " + myDouble);
+        System.out.println("Char = " + myChar);
+        System.out.println("String = " + myString);
+        
+    }
+}
+
+
+// DEFAULT VALUES
+// Byte = 0
+// Short = 0
+// Int = 0
+// Long = 0
+// Float = 0.0
+// Double = 0.0
+// Char =
+// String = null
+// MODIFIED VALUES
+// Byte = 3
+// Short = 35
+// Int = 35
+// Long = 25
+// Float = 25.0
+// Double = 32.0
+// Char = c
+// String = this is a string
