508aab5ac32fca6a9764462b20af1a5ae108f8db
gaurav garje <gauravgarje2000@gmail.com>
2022-02-20 22:52:34 +0530

    LeapYear.java modification

--- LeapYear.java
+++ LeapYear.java
@@ -5,15 +5,30 @@ public class LeapYear {
         Scanner scan = new Scanner(System.in);
         int year = scan.nextInt();
 
-        if (year % 400 == 0) {
-            System.out.println("leap year");
-            System.exit(0);
-        } else if ((year % 100 == 0) && (year % 4 == 0)) {
-            System.out.println("leap year");
-            System.exit(0);
-        } else {
-            System.out.println("Not leap year");
+        if(year%4 == 0)
+        {
+            if(year%100 == 0)
+            {
+                if(year%400 == 0)
+                    System.out.println(year + " is a leap year");
+                else
+                    System.out.println(year + " is not a leap year");
+            }
+            else
+                System.out.println(year + " is a leap year");
         }
+        else
+            System.out.println(year + "is not a leap year");
+
+        // if (year % 400 == 0) {
+        //     System.out.println("leap year");
+        //     System.exit(0);
+        // } else if ((year % 100 == 0) && (year % 4 == 0)) {
+        //     System.out.println("leap year");
+        //     System.exit(0);
+        // } else {
+        //     System.out.println("Not leap year");
+        // }
 
         scan.close();
     }
